d3ff64b7a24bbe259fb345f779f4acc73ef85efe
EQwon <bluetwintail0906@gmail.com>
2019-08-30 14:29:40 +0900

    Create save load system

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data
+{
+    public int stage;
+    public int mp;
+    public int[] affinity;
+
+    public Data(Data data)
+    {
+        stage = data.stage;
+        mp = data.mp;
+
+        affinity = new int[8];
+        for (int i = 0; i < affinity.Length; i++)
+        {
+            affinity[i] = data.affinity[i];
+        }
+    }
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveData(Data data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = Application.persistentDataPath + "data.txt";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        Data newData = new Data(data);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static Data LoadData()
+    {
+        string path = Application.persistentDataPath + "data.txt";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            Data data = formatter.Deserialize(stream) as Data;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
