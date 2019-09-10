4a596c5d85ac5e3f11ffd2d7f7b833af3cba6b72
EQwon <bluetwintail0906@gmail.com>
2019-09-10 13:46:53 +0900

    파싱 테스트 완료

---
+++ Assets/Resources/Parser.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Parser
+{
+    public static List<List<List<string>>> DialogParse(TextAsset data)
+    {
+        List<List<List<string>>> returnList = new List<List<List<string>>>();
+        StringReader sr = new StringReader(data.text);
+
+        bool isEnd = false;
+        while (isEnd == false)
+        {
+            bool readMode = false;
+
+            while (readMode == false)       // ===가 나올때까지 읽는다.
+            {
+                string findStart = sr.ReadLine();
+                if (findStart == "===")
+                {
+                    readMode = true;
+                }
+                if (findStart == "END")
+                {
+                    isEnd = true;
+                    break;
+                }
+            }
+
+            if (isEnd == true) break;
+
+            string source = sr.ReadLine();  // 먼저 한줄을 읽는다. 
+            string[] values;                // 구분된 데이터들을 저장할 배열 (values[0]이면 첫번째 데이터 )
+            List<List<string>> dialog = new List<List<string>>();   // 하나의 대화를 저장하는 2차원 리스트
+
+            while (source != "===")         //===를 만나면 그만둔다.
+            {
+                values = source.Split('/');  // Slash로 구분한다
+
+                List<string> temp = new List<string>();
+                for (int i = 0; i < values.Length; i++)
+                {
+                    temp.Add(values[i]);
+                }
+                dialog.Add(temp);
+
+                source = sr.ReadLine();    // 한줄 읽는다.
+            }
+
+            returnList.Add(dialog);
+        }
+
+        return returnList;
+    }
+}
---
+++ Assets/Resources/ParsingTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParsingTest : MonoBehaviour
+{
+    public TextAsset dialogue;
+
+    private void Start()
+    {
+        List<List<List<string>>> test = new List<List<List<string>>>();
+        test = Parser.DialogParse(dialogue);
+
+        for (int j = 0; j < test.Count; j++)
+        {
+            for (int i = 0; i < test[j].Count; i++)
+            {
+                string name = test[j][i][0];
+                string conversation = test[j][i][1];
+
+                Debug.Log(j + "번째 대화 - " + name + " : " + conversation);
+            }
+        }
+    }
+}
