e0aef3ddaedd2183d62dda9ecca96e75afb327b8
EQwon <bluetwintail0906@gmail.com>
2019-11-16 21:01:25 +0900

    땡땡이 퀘스트 구현 밑준비

---
+++ Assets/Scripts/Quest/CarrotTalkSizeFitter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarrotTalkSizeFitter : MonoBehaviour
+{
+    [SerializeField] private RectTransform textBox;
+    [SerializeField] private Text text;
+
+    private float originX;
+
+    private void Start()
+    {
+        originX = textBox.sizeDelta.x;
+    }
+
+    private void Update()
+    {
+        textBox.sizeDelta = new Vector2(originX, text.GetComponent<RectTransform>().sizeDelta.y + 30);
+    }
+
+    public void AssignText(string script)
+    {
+        text.text = script;
+    }
+}
---
+++ Assets/Scripts/Quest/Skipping.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skipping : MonoBehaviour
+{
+    [Header("Object Holder")]
+    [SerializeField] private GameObject phoneScreen;
+    [SerializeField] private List<GameObject> answerButtons;
+
+    [Header("Resource Holder")]
+    [SerializeField] private GameObject teacherTextPrefab;
+    [SerializeField] private GameObject myTextPrefab;
+    [SerializeField] private TextAsset skippingTextAsset;
+
+    private void Start()
+    {
+    }
+
+    private void TeacherSpeak(string script)
+    {
+        GameObject teacherText = Instantiate(teacherTextPrefab, phoneScreen.transform);
+
+        teacherText.GetComponent<CarrotTalkSizeFitter>().AssignText(script);
+    }
+
+    private void MySpeak(string script)
+    {
+        GameObject myText = Instantiate(myTextPrefab, phoneScreen.transform);
+
+        myText.GetComponent<CarrotTalkSizeFitter>().AssignText(script);
+    }
+
+    public void Answer(int i)
+    {
+
+    }
+}
