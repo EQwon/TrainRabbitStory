8f908b77216e4ba5ef634046634d75d4516819fd
EQwon <bluetwintail0906@gmail.com>
2020-02-25 22:22:19 +0900

    DOTween을 추가해서 시작화면의 지하철 이동

--- Assets/Scripts/Manager/StartManager.cs
+++ Assets/Scripts/Manager/StartManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class StartManager : MonoBehaviour
 {
     [Header("Holder")]
-    public RectTransform Train;
+    public RectTransform train;
     public GameObject stageName;
 
     [Header("Resources")]
@@ -31,35 +32,21 @@ public class StartManager : MonoBehaviour
     public void StartGame()
     {
         stageName.GetComponent<Button>().enabled = false;
-        StartCoroutine(MoveTrain());
 
-        GameManager.instance.LoadData();
+        float y = train.anchoredPosition.y;
+        Vector2 rectPos = new Vector2(0, y);
+        Vector2 endPos = new Vector2(-2000, y);
+
+        Sequence trainSequence = DOTween.Sequence();
+        trainSequence.Append(DOTween.To(() => train.anchoredPosition, x => train.anchoredPosition = x, rectPos, 2f).SetEase(Ease.OutQuart));
+        trainSequence.AppendInterval(1f);
+        trainSequence.Append(DOTween.To(() => train.anchoredPosition, x => train.anchoredPosition = x, endPos, 2f).SetEase(Ease.InQuart));
+        trainSequence.AppendCallback(() => LoadGameScene());
     }
 
-    private IEnumerator MoveTrain()
+    private void LoadGameScene()
     {
-        Vector3 targetPos = new Vector3(0, Train.localPosition.y, Train.localPosition.z);
-        float speed = 1500f;
-
-        while (Vector3.Distance(Train.localPosition, targetPos) > Mathf.Epsilon)
-        {
-            Train.localPosition = Vector3.MoveTowards(Train.localPosition, targetPos, Time.fixedDeltaTime * speed);
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
-        }
-
-        yield return new WaitForSeconds(2f);
-
-        targetPos -= new Vector3(2000, 0, 0);
-
-        while(Vector3.Distance(Train.localPosition, targetPos) > Mathf.Epsilon)
-        {
-            Train.localPosition = Vector3.MoveTowards(Train.localPosition, targetPos, Time.fixedDeltaTime * speed);
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
-        }
-
-        yield return new WaitForSeconds(0.5f);
-
-        //Debug.Log("게임을 새로 시작합니다.");
+        GameManager.instance.LoadData();
         SceneManager.LoadScene(nowStage + 1);
     }
 }
